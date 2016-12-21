c25fed5484edb920916527aa38c149d44d7c439e
dfenjves <fenjves.daniel@gmail.com>
2016-12-21 15:12:07 -0500

    added cards

--- app/components/Representative.js
+++ app/components/Representative.js
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom')
 var Materialize = require('react-materialize');
 var Modal = Materialize.Modal;
 var Button = Materialize.Button;
+var Card = Materialize.Card;
 
 class Representative extends React.Component {
 
@@ -18,8 +19,10 @@ class Representative extends React.Component {
           header={`${this.props.first_name} ${this.props.last_name}`}
           trigger={
             <div>
+            <Card className='blue-grey darken-1' textClassName='white-text' title='Card title'>
               <img src={this.createImageLink()} />
-              <h2> Name:{this.props.first_name} {this.props.last_name}</h2>
+              <h4> Name:{this.props.first_name} {this.props.last_name}</h4>
+            </Card>
             </div>
           }>
           <p>Address:{this.props.office}, Washington, DC 20002</p>
--- app/containers/RepresentativesContainer.js
+++ app/containers/RepresentativesContainer.js
@@ -1,17 +1,25 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 var Representative = require('../components/representative')
+var Materialize = require('react-materialize');
+var Row = Materialize.Row;
+var Col = Materialize.Col;
+
 
 
 class RepresentativesContainer extends React.Component {
   render(){
     return(
       <div>
-      {
-        this.props.representatives.map((rep) =>
-          <Representative first_name={rep.first_name} last_name={rep.last_name} office={rep.office} bioguide_id={rep.bioguide_id}/>
-        )
-      }
+        <Row>
+        {
+          this.props.representatives.map((rep) =>
+            <Col m={6} s={12}>
+              <Representative first_name={rep.first_name} last_name={rep.last_name} office={rep.office} bioguide_id={rep.bioguide_id}/>
+            </Col>
+          )
+        }
+        </Row>
       </div>
   )
   }
